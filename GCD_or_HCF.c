a82a7b0a9f375b78ad140e755f6bb39fff33f7a2
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-04-14 17:04:40 +0530

    #include<stdio.h> int main() {     int a,b,t,i=0;     scanf("%d%d",&a,&b);     for(t=2;t<=a&&t<=b;t++)     {         if(a%t==0&&b%t==0)         (i=t);     }     if(i!=0)     printf("%d",i);     else     printf("1");     return 0; }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,t,i=0;
+    scanf("%d%d",&a,&b);
+    for(t=2;t<=a&&t<=b;t++)
+    {
+        if(a%t==0&&b%t==0)
+        (i=t);
+    }
+    if(i!=0)
+    printf("%d",i);
+    else
+    printf("1");
+    return 0;
+}
