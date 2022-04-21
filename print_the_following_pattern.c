a79f76f135b416c6387feffc9f995f09890a4450
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-04-21 22:22:03 +0530

    #include<stdio.h> int main() {     int N,i,j;     scanf("%d",&N);     for(i=1;i<=N;i++)     {         for(j=1;j<=N;j++)         {             printf("%c ",(i+64));         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,9 +5,9 @@ int main()
     scanf("%d",&N);
     for(i=1;i<=N;i++)
     {
-        for(j=N;j>=1;j--)
+        for(j=1;j<=N;j++)
         {
-            printf("%d ",j);
+            printf("%c ",(i+64));
         }
         printf("
 ");
