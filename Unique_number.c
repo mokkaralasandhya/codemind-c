d89a72183c0615c3aa369c4fb245bbb0c35625a5
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-04-22 21:11:39 +0530

    #include<stdio.h> int main() {     int n,m,temp,one=0,two=0,three=0,four=0,five=0,six=0,seven=0,eight=0,nine=0,zero=0;     scanf("%d",&n);     temp=n;     while(n>0)     {         m=n%10;         if(m==1){one++;}         if(m==2){two++;}         if(m==3){three++;}         if(m==4){four++;}         if(m==5){five++;}         if(m==6){six++;}         if(m==7){seven++;}         if(m==8){eight++;}         if(m==9){nine++;}         if(m==10){zero++;}         n/=10;     }     if(one<=1 && two<=1 && three<=1 && four<=1 && five<=1 && six<=1 && seven<=1 && eight<=1 && nine<=1 && zero<=1)     {         printf("Unique Number");     }     else     {         printf("Not Unique Number");     } }

--- Unique_number.c
+++ Unique_number.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,digit;
-    static int arr[10];
+    int n,m,temp,one=0,two=0,three=0,four=0,five=0,six=0,seven=0,eight=0,nine=0,zero=0;
     scanf("%d",&n);
+    temp=n;
     while(n>0)
     {
-        digit=n%10;
-        arr[digit]++;
-        n=n/10;
+        m=n%10;
+        if(m==1){one++;}
+        if(m==2){two++;}
+        if(m==3){three++;}
+        if(m==4){four++;}
+        if(m==5){five++;}
+        if(m==6){six++;}
+        if(m==7){seven++;}
+        if(m==8){eight++;}
+        if(m==9){nine++;}
+        if(m==10){zero++;}
+        n/=10;
     }
-    for(i=0;i<10;i++)
-    {
-        if(arr[i]>1)
-        {
-            printf("Not Unique Number");
-            break;
-        }
-    }
-    if(i==10)
+    if(one<=1 && two<=1 && three<=1 && four<=1 && five<=1 && six<=1 && seven<=1 && eight<=1 && nine<=1 && zero<=1)
     {
         printf("Unique Number");
     }
-    return 0;
+    else
+    {
+        printf("Not Unique Number");
+    }
 }
