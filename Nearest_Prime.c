e9668e58fb240cdf40c5fd98e299b03b3bf2f41d
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-05-22 13:10:05 +0530

    #include<stdio.h> int main() {     int n,i,a,c=0,c1=0,r1,r2,k,min,max,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a);         for(j=a;j>=0;j--)         {             c=0;             for(k=1;k<=j;k++)             {               if(j%k==0)               {                 c++;               }             }          if(c==2)           {              min=j;              break;           }         }      for(j=a;;j++)      {         c1=0;         for(k=1;k<=j;k++)         {             if(j%k==0)             {                 c1++;             }         }         if(c1==2)         {             max=j;             break;         }      }     r1=a-min;     r2=max-a;     if(r1<r2)     printf("%d ",min);     else if(r1==r2)     printf("%d ",min);     else     printf("%d ",max);     } }

---
+++ Nearest_Prime.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,c=0,c1=0,r1,r2,k,min,max,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a);
+        for(j=a;j>=0;j--)
+        {
+            c=0;
+            for(k=1;k<=j;k++)
+            {
+              if(j%k==0)
+              {
+                c++;
+              }
+            }
+         if(c==2)
+          {
+             min=j;
+             break;
+          }
+        }
+     for(j=a;;j++)
+     {
+        c1=0;
+        for(k=1;k<=j;k++)
+        {
+            if(j%k==0)
+            {
+                c1++;
+            }
+        }
+        if(c1==2)
+        {
+            max=j;
+            break;
+        }
+     }
+    r1=a-min;
+    r2=max-a;
+    if(r1<r2)
+    printf("%d
+",min);
+    else if(r1==r2)
+    printf("%d
+",min);
+    else
+    printf("%d
+",max);
+    }
+}
