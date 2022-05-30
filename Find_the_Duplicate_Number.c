2010b8d35b73715518295d80eaece83a57944ea5
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-05-30 17:20:50 +0530

    #include<stdio.h> int main() {     int n,a[100];     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     { int c=0;         for(int j=i+1;j<n;j++)         {             if(a[i]==a[j])             c++;         }         if(c>0)         printf("%d ",a[i]);     } }

---
+++ Find_the_Duplicate_Number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    { int c=0;
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            c++;
+        }
+        if(c>0)
+        printf("%d ",a[i]);
+    }
+}
