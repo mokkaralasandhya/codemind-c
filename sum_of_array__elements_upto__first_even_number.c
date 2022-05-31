c59ccd7b7a8ada290a9295b8ee1d8beef476d474
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-05-31 21:59:37 +0530

    #include<stdio.h> int main() {     int ar[100],n,k,sum=0;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     scanf("%d",&k);     for(int i=0;i<n;i++)     {         if(ar[i]%2!=0)         sum=sum+ar[i];         else         break;     }     printf("%d",sum); }

---
+++ sum_of_array__elements_upto__first_even_number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int ar[100],n,k,sum=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    scanf("%d",&k);
+    for(int i=0;i<n;i++)
+    {
+        if(ar[i]%2!=0)
+        sum=sum+ar[i];
+        else
+        break;
+    }
+    printf("%d",sum);
+}
+
