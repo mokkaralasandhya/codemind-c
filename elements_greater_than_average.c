00a3dc1d5dd08659f6b2c61729d7ba72f742529b
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-05-31 21:00:13 +0530

    #include<stdio.h> int main() {     int ar[100],n,sum=0,c=0;     int a;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&ar[i]);         sum=sum+ar[i];     }     a=(sum/n);     for(int i=0;i<n;i++)     {         if(ar[i]>=a)         c++;     }     printf("%d",c); }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int ar[100],n,sum=0,c=0;
+    int a;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+        sum=sum+ar[i];
+    }
+    a=(sum/n);
+    for(int i=0;i<n;i++)
+    {
+        if(ar[i]>=a)
+        c++;
+    }
+    printf("%d",c);
+}
+
