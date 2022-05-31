f1c03edbdd4467889826945bda55039e984fc1dc
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-05-31 21:54:10 +0530

    #include<stdio.h> int main() {     int n,a,b,ar[100],min=99,c=0;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     scanf("%d%d",&a,&b);     for(int i=0;i<n;i++)     {         if(!(ar[i]>=a&&ar[i]<=b))         {           if(ar[i]<min)           {           min=ar[i];           c++;           }         }     }     if(c==0)     printf("-1");     else     printf("%d",min); }

---
+++ minimum_not_between_A_and_B.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b,ar[100],min=99,c=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(int i=0;i<n;i++)
+    {
+        if(!(ar[i]>=a&&ar[i]<=b))
+        {
+          if(ar[i]<min)
+          {
+          min=ar[i];
+          c++;
+          }
+        }
+    }
+    if(c==0)
+    printf("-1");
+    else
+    printf("%d",min);
+}
+
