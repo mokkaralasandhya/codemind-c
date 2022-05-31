16f71921ea0bcdbde26ed463c0009374a8b05147
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-05-31 21:26:49 +0530

    #include<stdio.h> int main() {     int n,a[100],f=0;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     {         int c=0;         for(int j=0;j<n;j++)         {             if(a[i]==a[j])             {                 c++;             }         }         if(c==a[i])         {             f++;             a[i]=0;         }     }     printf("%d",f); }

---
+++ value_same_as_count.c
@@ -0,0 +1,32 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,a[100],f=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        int c=0;
+        for(int j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(c==a[i])
+        {
+            f++;
+            a[i]=0;
+        }
+    }
+    printf("%d",f);
+}
+
+
+
+
