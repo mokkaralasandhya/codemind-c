7b39578b3f180d32743ad5c0cc817f0d616cda24
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-06-01 09:55:06 +0530

    #include<stdio.h> int main() {     int a[100],n,c=0;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }       for(int i=0;i<n;i++)     {         for(int j=i+1;j<n;j++)         {             if(a[j]<=a[i])             {                 c++;             }         }     }     if(c==0)     {         printf("yes");     }     else     printf("no"); }

---
+++ Ascending_array.c
@@ -0,0 +1,47 @@
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a[100],n,c=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+      for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]<=a[i])
+            {
+                c++;
+            }
+        }
+    }
+    if(c==0)
+    {
+        printf("yes");
+    }
+    else
+    printf("no");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
