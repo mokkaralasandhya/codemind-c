4d8511ef892a7bdc4974e83a7dcfc9d297d0fc42
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-07-10 18:14:34 +0530

    #include<stdio.h> #include<math.h> int main() {     int i,n,flag,c;     int l,r;     scanf("%d",&l);     scanf("%d",&r);     c=0;     for(n=l;n<=r;++n)     {         if(n==1)         continue;         flag=0;         for(i=2;i<=sqrt(n);++i)         {             if(n%i==0)             {                 flag=1;                 break;             }         }         if(flag==0)         ++c;     }     printf("%d",c); }

--- Primes_in_range.c
+++ Primes_in_range.c
@@ -1,37 +1,27 @@
 #include<stdio.h>
 #include<math.h>
-int factorcount(int i)
-{
-    int j,fc=2,sq;
-    sq=sqrt(i);
-    if(i==1)
-    {
-        return 1;
-    }
-    for(j=2;j<=sq;j++)
-    {
-        if(i%j==0)
-        {
-            fc+=2;
-        }
-    }
-    if(sq*sq==i)
-    {
-        fc-=i;
-    }
-    return fc;
-}
 int main()
 {
-    int i,n1,n2,fc,p=0;
-    scanf("%d%d",&n1,&n2);
-    for(i=n1;i<=n2;i++)
+    int i,n,flag,c;
+    int l,r;
+    scanf("%d",&l);
+    scanf("%d",&r);
+    c=0;
+    for(n=l;n<=r;++n)
     {
-        fc=factorcount(i);
-        if(fc==2)
+        if(n==1)
+        continue;
+        flag=0;
+        for(i=2;i<=sqrt(n);++i)
         {
-            p++;
+            if(n%i==0)
+            {
+                flag=1;
+                break;
+            }
         }
+        if(flag==0)
+        ++c;
     }
-    printf("%d",p);
+    printf("%d",c);
 }
