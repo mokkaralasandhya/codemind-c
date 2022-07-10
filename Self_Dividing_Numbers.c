28dc858e38d45a1a7ef52170691167f806903428
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-07-10 18:46:17 +0530

    #include<stdio.h> int main() {     int m,i,r=0,n,s=0,t,c=0;     scanf("%d%d",&m,&n);     for(i=m;i<=n;i++)     {         c=0;         t=i;         while(t)         {             r=t%10;             if((r==0)||(i%r!=0))             {                 c++;             }             t/=10;         }         if(c==0)         {             printf("%d ",i);         }     } }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,32 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i,num,r,temp,sum,count;
-    scanf("%d%d",&a,&b);
-    for(i=a;i<=b;i++)
+    int m,i,r=0,n,s=0,t,c=0;
+    scanf("%d%d",&m,&n);
+    for(i=m;i<=n;i++)
     {
-        if(i%10!=0)
+        c=0;
+        t=i;
+        while(t)
         {
-            sum=0;count=0;num=i;temp=i;
-            while(num>0)
+            r=t%10;
+            if((r==0)||(i%r!=0))
             {
-                r=num%10;
-                sum++;
-                if(r==0)
-                {
-                    break;
-                }
-                if(temp%r==0)
-                {
-                    count++;
-                }
-                num/=10;
+                c++;
             }
-            if(sum==count)
-            {
-                printf("%d ",i);
-            }
+            t/=10;
+        }
+        if(c==0)
+        {
+            printf("%d ",i);
         }
     }
-    return 0;
 }
