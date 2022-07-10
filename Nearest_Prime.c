168d2d04b1ad422abf81cfd92c280813edcd468c
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-07-10 21:28:31 +0530

    #include<stdio.h> int prime(int num) {     int i;     for(i=2;i*i<=num;i++)     {         if(num%i==0)         {             return 0;         }     }     return 1; } int main() {     int a;     scanf("%d",&a);     int arr[a];     int diff1=0,diff2=0,i,j,p1,p2;     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<a;i++)     {         for(j=arr[i];j<=10000;j++)         {             if(prime(j)==1)             {                 diff1=j-arr[i];                 p1=j;                 break;             }         }         for(j=arr[i];j>0;j--)         {             if(prime(j)==1)             {                 diff2=arr[i]-j;                 p2=j;                 break;             }         }         if(diff1>diff2)         {             printf("%d",p2);         }         else if(diff1<diff2)         {             printf("%d",p1);         }         else         {             if(p1>p2)             {                 printf("%d",p2);             }             else             {                 printf("%d",p1);             }         }         printf(" ");     }     return 0; }

--- Nearest_Prime.c
+++ Nearest_Prime.c
@@ -1,53 +1,67 @@
 #include<stdio.h>
+int prime(int num)
+{
+    int i;
+    for(i=2;i*i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
 int main()
 {
-    int n,i,a,c=0,c1=0,r1,r2,k,min,max,j;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    int diff1=0,diff2=0,i,j,p1,p2;
+    for(i=0;i<a;i++)
     {
-        scanf("%d",&a);
-        for(j=a;j>=0;j--)
-        {
-            c=0;
-            for(k=1;k<=j;k++)
-            {
-                if(j%k==0)
-                {
-                    c++;
-                }
-            }
-            if(c==2)
-            {
-                min=j;
-                break;
-            }
-        }
-        for(j=a;;j++)
-        {
-            c1=0;
-            for(k=1;k<=j;k++)
-            {
-                if(j%k==0)
-                {
-                    c1++;
-                }
-            }
-            if(c1==2)
-            {
-                max=j;
-                break;
-            }
-        }
-        r1=a-min;
-        r2=max-a;
-        if(r1<r2)
-        printf("%d
-",min);
-        else if(r1==r2)
-        printf("%d
-",min);
-        else
-        printf("%d
-",max);
+        scanf("%d",&arr[i]);
     }
+    for(i=0;i<a;i++)
+    {
+        for(j=arr[i];j<=10000;j++)
+        {
+            if(prime(j)==1)
+            {
+                diff1=j-arr[i];
+                p1=j;
+                break;
+            }
+        }
+        for(j=arr[i];j>0;j--)
+        {
+            if(prime(j)==1)
+            {
+                diff2=arr[i]-j;
+                p2=j;
+                break;
+            }
+        }
+        if(diff1>diff2)
+        {
+            printf("%d",p2);
+        }
+        else if(diff1<diff2)
+        {
+            printf("%d",p1);
+        }
+        else
+        {
+            if(p1>p2)
+            {
+                printf("%d",p2);
+            }
+            else
+            {
+                printf("%d",p1);
+            }
+        }
+        printf("
+");
+    }
+    return 0;
 }
