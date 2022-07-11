885c1308e2506f55b80bf59ec09e6e7420fd1385
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-07-11 19:11:18 +0530

    #include<stdio.h> #include<math.h> int size(int num) {     int c=0;     while(num>0)     {         c++;         num/=10;     }     return c; } int rev(int num) {     int s=0,r;     while(num>0)     {         r=num%10;         s=(s*10)+r;         num/=10;     }     return s; } int main() {     int a;     scanf("%d",&a);     int ar=rev(a);     int i,sum=0,c=size(a),j=1;     while(ar>0)     {         int dig=ar%10;         sum+=pow(dig,j);         j++;         ar/=10;     }     if(sum==a)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

--- Disarium_number.c
+++ Disarium_number.c
@@ -1,25 +1,40 @@
 #include<stdio.h>
 #include<math.h>
+int size(int num)
+{
+    int c=0;
+    while(num>0)
+    {
+        c++;
+        num/=10;
+    }
+    return c;
+}
+int rev(int num)
+{
+    int s=0,r;
+    while(num>0)
+    {
+        r=num%10;
+        s=(s*10)+r;
+        num/=10;
+    }
+    return s;
+}
 int main()
 {
-    int N,m,te1,te2,count=0,sum=0;
-    scanf("%d",&N);
-    te1=N;
-    te2=N;
-    while(N>0)
+    int a;
+    scanf("%d",&a);
+    int ar=rev(a);
+    int i,sum=0,c=size(a),j=1;
+    while(ar>0)
     {
-        m=N%10;
-        count++;
-        N/=10;
+        int dig=ar%10;
+        sum+=pow(dig,j);
+        j++;
+        ar/=10;
     }
-    while(te1>0)
-    {
-        m=te1%10;
-        sum+=pow(m,count);
-        te1/=10;
-        count--;
-    }
-    if(sum==te2)
+    if(sum==a)
     {
         printf("True");
     }
