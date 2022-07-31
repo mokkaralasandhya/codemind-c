ef176551cdf699642cc2f5d31e137f0e4b87f3c9
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-07-31 08:59:39 +0530

    #include<stdio.h> int main() {     int n,a[100],sum=0,f=0;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     {         int c=0;         for(int j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                 c++;             }         }         if(c==0)         {             sum+=a[i];         }     }     printf("%d",sum); }

--- unique_sum.c
+++ unique_sum.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],f=0,sum=0;
+    int n,a[100],sum=0,f=0;
     scanf("%d",&n);
     for(int i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
     for(int i=0;i<n;i++)
-    { int c=0;
+    {
+        int c=0;
         for(int j=i+1;j<n;j++)
         {
             if(a[i]==a[j])
@@ -18,7 +19,7 @@ int main()
         }
         if(c==0)
         {
-            sum=sum+a[i];
+            sum+=a[i];
         }
     }
     printf("%d",sum);
